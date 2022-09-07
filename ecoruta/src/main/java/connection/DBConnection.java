3425439a5652092ebdaebe11ed4fa6d4ce07fbb3
Ronald <ronaldv80@gmail.com>
2022-09-07 15:50:11 -0500

    Conexion a la BD

---
+++ ecoruta/src/main/java/connection/DBConnection.java
@@ -0,0 +1,11 @@
+
+package connection;
+
+import com.mysql.jdbc.Connection;
+
+public class DBConnection {
+    Connection connection;
+    static String bd="XXXXXXXXXXX";
+    static String port="3306";
+    
+}
