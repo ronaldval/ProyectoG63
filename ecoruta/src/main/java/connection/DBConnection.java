b3cd209c6ef89e34f1b692740c265eba024bcd99
Beatriz <beatriz.gl@yahoo.com>
2022-09-26 13:26:57 -0500

    Cambios
    Estilos de las paginas

--- ecoruta/src/main/java/connection/DBConnection.java
+++ ecoruta/src/main/java/connection/DBConnection.java
@@ -5,7 +5,21 @@ import com.mysql.jdbc.Connection;
 
 public class DBConnection {
     Connection connection;
-    static String bd="XXXXXXXXXXX";
+    static String bd="xxxxx";
     static String port="3306";
+    static String login="root";
+    static String password="admin";
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:"+this.port+"/"+this.bd;
+            System.out.println("conexion establecida");
+            
+        } catch (Exception ex) {
+            System.out.println("error en la conexion");
+        }
+    }
+    
     
 }   
